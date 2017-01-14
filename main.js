7292b494c7d0f41e7c027fec8a20bdc6408ebef1
Karkoon <kacperak23@gmail.com>
2017-01-14 16:06:21 +0100

    Dodałem więcej okien, naprawiłem problemy z tym związane.

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
-  $(function () {
-  	$(".draggable").draggable().resizable({handles: 'all'});
+/*jslint browser: true*/
+/*global $, jQuery, alert*/
+$(function () {
+	"use strict";
+	$(".draggable").draggable().resizable({
+		handles: 'all'
+	});
+	$(".draggable").mousedown(function () {
+		var biggestZIndex = 0;
+		$(this).siblings('.draggable').each(function () {
+			var z_index = $(this).css('zIndex');
+			if (biggestZIndex < z_index) {
+				biggestZIndex = z_index;
+			}
+		});
+		$(this).css('z-index', biggestZIndex + 1);
+	});
 	$(".title").text($("iframe").attr("src"));
-  });
+});
